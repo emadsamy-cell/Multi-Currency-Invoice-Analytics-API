cd7e3f35df439981f34d715cab9f0a74fc17edc4
emadsamy-cell <samiemad567@gmail.com>
2025-11-10 10:00:50 +0200

    added exchange service

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,9 +1,3 @@
-"""
-Routers package for Multi-Currency Invoice application.
-
-This package contains all API route handlers organized by domain.
-"""
-
 from app.routers import customers
 
 __all__ = ["customers"]
